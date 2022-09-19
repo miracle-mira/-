5d3f58e41645735f1d7eba5705be0b23d2a4b222
박미라 <parkmira323@gmail.com>
2022-09-20 01:05:47 +0900

    Delete JavaPracice/HW1 directory

--- JavaPracice/HW1/TernaryOperator.java
+++
@@ -1,8 +0,0 @@
-//예제 2-8
-
-public class TernaryOperator {
-    public static void main (String[] args) {
-        int a = 3, b = 5;
-        System.out.println("두 수의 차는 " + ((a>b)?(a-b):(b-a)));
-    }
-}
