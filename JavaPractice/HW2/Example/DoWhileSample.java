edda311906d6248e9273f4ffb879d3591c3328f8
박미라 <parkmira323@gmail.com>
2022-09-27 12:40:42 +0900

    Add files via upload

---
+++ JavaPractice/HW2/Example/DoWhileSample.java
@@ -0,0 +1,11 @@
+//예제 3-3
+
+public class DoWhileSample {
+    public static void main (String[] args) {
+        char c = 'a';
+        do {
+            System.out.print(c);
+            c = (char) (c + 1);
+        } while (c <= 'z');
+    }
+}
